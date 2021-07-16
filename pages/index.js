a8c435019d6ebb57e3d9558af8b5c5238117a5d1
Rafaela Sacconi <rafaela.sacconi@luizalabs.com>
2021-07-16 19:11:56 -0300

    feat: include AlurakutStyles and AlurakutCommons

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,63 @@
 import MainGrid from './../src/components/MainGrid';
 import Box from './../src/components/Box';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-
-function ProfileSidebar(propriedades) {
+function ProfileSidebar(properties) {
   return (
     <Box>
-      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <img src={`https://github.com/${properties.githubUser}.png`} style={{ borderRadius: '8px' }} />
     </Box>
   )
 }
 
 export default function Home() {
   const githubUser = 'rafaelassacconi';
+  const favoritePeople = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
 
   return (
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-        <Box>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
           <ProfileSidebar githubUser={githubUser} />
-        </Box>
-      </div>
-      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
-        <Box>
-            Bem vindo
-        </Box>
-      </div>
-      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-        <Box>
-          Pessoas da comunidade
-        </Box>
-      </div>
-    </MainGrid>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a) 
+            </h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({favoritePeople.length})
+            </h2>
+
+            <ul>
+              {favoritePeople.map((person) => {
+                return (
+                  <li key={person}>
+                    <a href={`/users/${person}`} key={person}>
+                      <img src={`https://github.com/${person}.png`} />
+                      <span>{person}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
   )
 }
