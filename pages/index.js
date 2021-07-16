b916e0a2d0435ac9aa9556f98f5699f34e412f14
Rafaela Sacconi <rafaela.sacconi@luizalabs.com>
2021-07-16 19:37:33 -0300

    feat: add profile links and form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import MainGrid from './../src/components/MainGrid';
 import Box from './../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(properties) {
   return (
     <Box>
       <img src={`https://github.com/${properties.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${properties.githubUser}`}>
+          @{properties.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />      
     </Box>
   )
 }
@@ -34,8 +44,45 @@ export default function Home() {
             <h1 className="title">
               Bem vindo(a) 
             </h1>
-            <OrkutNostalgicIconSet />
+            <OrkutNostalgicIconSet 
+              recados={305}
+              fotos={56}
+              videos={2}
+              fas={1}
+              mensagens={7}
+              confiavel={3}
+              legal={3}
+              sexy={3}
+            />
           </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+
+            <form>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                  />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+
+              <button>
+                Criar comunidade
+              </button>
+            </form>
+
+          </Box>
+
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
           <ProfileRelationsBoxWrapper>
